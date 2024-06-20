ca7c4d628befb294c9a38a6216ea31b9cb9d6684
tejas <tejas0215lc@gmail.com>
2024-06-20 14:41:07 +0530

    Plugin Changed

---
+++ wdm-electsure-learndash-customization/admin/partials/wdm-electsure-learndash-customization-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://wisdmlabs.com
+ * @since      1.0.0
+ *
+ * @package    Wdm_Electsure_Learndash_Customization
+ * @subpackage Wdm_Electsure_Learndash_Customization/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
