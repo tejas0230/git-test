8387d4eea4df18f1a0f0a04484f2f9f27dab1662
tejas <tejas0215lc@gmail.com>
2024-06-21 09:57:32 +0530

    New commit

--- wdm-electsure-learndash-customization/public/partials/class-test.php
+++ wdm-electsure-learndash-customization/public/partials/class-test.php
@@ -1,24 +1,43 @@
 <?php
+/**
+ * Public-facing view for the Wdm Electsure Learndash Customization plugin.
+ *
+ * This file contains the definition for the Test class used in the plugin.
+ *
+ * @link       https://wisdmlabs.com
+ * @since      1.0.0
+ *
+ * @package    Wdm_Electsure_Learndash_Customization
+ * @subpackage Wdm_Electsure_Learndash_Customization/public/partials
+ */
 
-class Test
-{
-    function __construct()
-    {
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
-        add_action( 'asdfasdfasdf', array('asdfasdfasdf'));
+/**
+ * Class Test
+ *
+ * A test class for demonstrating plugin functionality.
+ *
+ * @since 1.0.0
+ */
+class Test {
 
-
-
-
-
-
-        
-    }
+	/**
+	 * Constructor.
+	 *
+	 * Initializes the class by adding actions.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+		add_action( 'asdfasdfasdf', array( 'asdfasdfasdf' ) );
+	}
 }
+
+// Instantiate the Test class.
 $test = new Test();
